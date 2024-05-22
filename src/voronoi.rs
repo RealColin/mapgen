82ee48338985b89194d3f429c65c54d9825fdfc4
Werner, Colin <crwgond@gmail.com>
2024-05-22 00:40:02 -0500

    stubbed voronoi

--- src/voronoi.rs
+++ src/voronoi.rs
@@ -7,4 +7,15 @@ impl Voronoi {
     pub fn init(seed: u64, square_size: u32) -> Self {
         Voronoi { seed, square_size }
     }
+
+    /* Get the cell that the global pos (x, y) belongs to */
+    pub fn get_cell_at(&self, x: i64, y: i64) -> (i64, i64) {
+        // TODO implement this function
+        (0, 0)
+    }
+
+    /* Get the site belonging to the square */
+    fn get_site_at(&self, gx: i64, gy: i64) {
+        // TODO implement this function
+    }
 }
