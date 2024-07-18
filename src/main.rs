34efc766c51097c6d1254a22de521c3ed503d277
Werner, Colin <crwgond@gmail.com>
2024-07-17 19:17:06 -0500

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use voronoi::Voronoi;
 
 fn main() {
     let vor = Voronoi::init(3, 64);
-    vor.gen_image(String::from("fortnite.png"), 1024);
+    vor.gen_image(String::from("image.png"), 1024);
 
     println!("Hello, world!");
 }
