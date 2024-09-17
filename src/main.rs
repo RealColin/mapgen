ea05c8e5431dcb1fc3fbf7cb0a9d6ec381a37ec0
Werner, Colin <crwgond@gmail.com>
2024-09-17 00:47:38 -0400

    yeah idk

--- src/main.rs
+++ src/main.rs
@@ -2,68 +2,60 @@ mod continent;
 mod map;
 mod voronoi;
 
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 use image::{GenericImage, GenericImageView, Rgb, RgbImage, Rgba, RgbaImage};
 use libnoise::{Generator, Source, Visualizer};
 use map::Map;
 use rand::{rngs::StdRng, seq::SliceRandom, Rng, SeedableRng};
+use voronoi::Voronoi;
 
 fn main() {
-    // let seed = 3;
-    // let size = 2048;
-
-    let fort = Source::simplex(0)
-        .scale([0.05, 0.05])
-        .fbm(4, 2.0, 2.0, 0.125)
-        .mul(8.0);
-    let nite = Source::simplex(1)
-        .scale([0.05, 0.05])
-        .fbm(4, 2.0, 2.0, 0.125)
-        .mul(8.0);
+    let vor = Voronoi::init(0, 16, 2);
+    let template = image::open("map.png").unwrap();
 
     let mut image = RgbaImage::new(2048, 2048);
-    let mut colors: HashMap<(u32, u32), Rgba<u8>> = HashMap::new();
+    let mut colors: HashMap<(i64, i64), Rgba<u8>> = HashMap::new();
 
-    let template = image::open("map.png").unwrap();
-    let dim = template.dimensions();
+    let fort = Source::perlin(0)
+        .scale([0.05, 0.05])
+        .fbm(8, 2.0, 2.0, 0.125)
+        .mul(8.0);
+    let nite = Source::perlin(1)
+        .scale([0.05, 0.05])
+        .fbm(8, 2.0, 2.0, 0.125)
+        .mul(8.0);
 
-    for x in 0..dim.0 {
-        for y in 0..dim.1 {
-            let color = template.get_pixel(x, y);
-            colors.insert((x, y), color);
-        }
-    }
+    // Visualizer::<2>::new([2048, 2048], &fort)
+    //     .write_to_file("fort.png")
+    //     .unwrap();
 
-    let seed: [u8; 32] = [0; 32];
-    let mut rng = StdRng::from_seed(seed);
+    let clamp = |val: f64| val.max(-4.0).min(4.0);
 
     for x in 0..2048 {
         for y in 0..2048 {
-            // let gx = x / 16;
-            // let gy = y / 16;
-            let sx = fort.sample([x as f64, y as f64]);
-            let sy = nite.sample([x as f64, y as f64]);
-            // println!("{sx}, {sy}");
+            let ox = clamp(fort.sample([x as f64 + 0.01, y as f64 + 0.01]));
+            let oy = clamp(nite.sample([x as f64 + 0.01, y as f64 + 0.01]));
 
-            let gx = ((x as f64 + sx) / 16.0) as u32;
-            let gy = ((y as f64 + sy) / 16.0) as u32;
-
-            if colors.contains_key(&(gx, gy)) {
-                image.put_pixel(x as u32, y as u32, *colors.get(&(gx, gy)).unwrap());
+            let site = vor.nearest_site((x as f64 + ox) as i64, (y as f64 + oy) as i64);
+            if colors.contains_key(&site) {
+                let color = colors.get(&site).unwrap();
+                image.put_pixel(x as u32, y as u32, *color);
             } else {
-                let color = Rgba([255, 255, 255, 255]);
-                colors.insert((gx, gy), color);
+                let site_box = (site.0 / 16, site.1 / 16);
+                let color;
+
+                if site_box.0 > 127 || site_box.1 > 127 || site_box.0 < 0 || site_box.1 < 0 {
+                    color = Rgba([0, 255, 0, 255]);
+                } else {
+                    color = template.get_pixel(site_box.0 as u32, site_box.1 as u32);
+                }
+
+                colors.insert(site, color);
                 image.put_pixel(x as u32, y as u32, color);
             }
         }
     }
 
-    // let _ = fort.sample([0.2, 1.3]);
-
-    // Visualizer::<2>::new([512, 512], &fort)
-    // .write_to_file("fort.png")
-    // .unwrap();
-
-    let _ = image.save("squares.png");
+    let _ = image.save("vorsq.png");
 }
